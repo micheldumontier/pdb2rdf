632bdb8be56e0dd7aa9972079910e63b84f62167
alexjdl <alexjdl@f50ddfa5-554b-e6f9-038f-235834ee804a>
2011-01-11 16:49:26 +0000

    make the log4j configuration an external file

--- pdb2rdf/trunk/pdb2rdf-cli/pdb2rdf.sh
+++ pdb2rdf/trunk/pdb2rdf-cli/pdb2rdf.sh
@@ -9,6 +9,6 @@ done
 MEMORY=2g
 
 #OPTS="-ea -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=8010"
-OPTS="-ea -Dlog4j.configuration=log4j.properties"
+OPTS="-ea -Dlog4j.configuration=file:log4j.properties"
 
 java -Xmx$MEMORY -cp $CLASSPATH $OPTS com.dumontierlab.pdb2rdf.Pdb2Rdf $@
